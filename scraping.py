49511498879404f788d894383c17580c5a4210b9
OPahunang <olypahunang@yahoo.com>
2022-03-13 00:04:36 -0500

    updated challenge files

--- scraping.py
+++ scraping.py
@@ -10,7 +10,6 @@ def scrape_all():
     # Set up Splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
-
     news_title, news_paragraph = mars_news(browser)
     
     # Run all scraping functions and store results in dictionary
@@ -19,7 +18,8 @@ def scrape_all():
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      "last_modified": dt.datetime.now(),
+      "hemispheres": mars_hemispheres(browser)
     }
 
     # Stop webdriver and return data
@@ -102,6 +102,38 @@ def mars_facts():
     
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+# ## Mars Hemishere
+def mars_hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'    
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    product_soup = soup(browser.html, 'html.parser')
+    product_list = product_soup     .find('div', class_='collapsible results')     .find_all('div', 'item')
+
+    # for each product visit the page, scrape data
+    for product in product_list:
+        # get link for product and visit page    
+        link = product.find('a').get('href')
+        browser.visit(f"{url}{link}")
+    
+        # get the html
+        hemi_soup = soup(browser.html, 'html.parser')
+    
+        # get the image title (only 1 h2 on page)  
+        hemi_title = hemi_soup.find('h2').text
+    
+        # get the image link (seek an anchor link labeled Sample)
+        hemi_url = hemi_soup.find('a', string="Sample").get('href')
+        hemisphere_image_urls.append({
+            'img_url': f"{url}{hemi_url}",
+            'title': hemi_title })
+        browser.back()
 
 if __name__ == "__main__":
     # If running as script, print scraped data
